ddbc9890df86e31926fbf7c65db89a97f744ca2e
viktoriaspichak <vspichak@ukr.net>
2023-01-29 18:03:42 +0200

    adaptation

--- js/scripts.js
+++ js/scripts.js
@@ -21,3 +21,17 @@ $(document).ready(function(){
         $( "#accordion" ).accordion();
     } );
 });
+
+const openMobileMenuBtn = document.querySelector(".device-menu");
+const headerMenu = document.querySelector(".header-nav");
+
+openMobileMenuBtn.addEventListener("click", () => {
+    if(openMobileMenuBtn.classList.contains("open")) {
+        openMobileMenuBtn.classList.remove("open");
+        headerMenu.classList.remove("active");
+    } else {
+        headerMenu.classList.add("active");
+        openMobileMenuBtn.classList.add("open");
+    }
+})
+
